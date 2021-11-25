a1b92333bee817ee45c5830b174dee0866438511
Steve Witman <stevewitman@gmail.com>
2021-11-24 23:57:18 -0700

    add forgot-password template

--- src/app/core/components/auth-forgot-password/auth-forgot-password.component.ts
+++ src/app/core/components/auth-forgot-password/auth-forgot-password.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth-forgot-password',
   templateUrl: './auth-forgot-password.component.html',
-  styleUrls: ['./auth-forgot-password.component.scss']
+  styleUrls: ['./auth-forgot-password.component.scss'],
 })
 export class AuthForgotPasswordComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onForgotPassword(email: string) {
+    this.router.navigate(['forgot-password']);
   }
-
 }
